4aae557dc1d925227814b4135d38a6126c647f2b
vmarce05 <39540711+vmarce05@users.noreply.github.com>
2018-09-25 17:01:39 -0500

    Update conex.php

--- conex.php
+++ conex.php
@@ -1,15 +1,15 @@
 <?php
-$dbhost = getenv("MYSQL_SERVICE_HOST");
-$dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("databaseuser");
-$dbpwd = getenv("databasepassword");
-$dbname = getenv("databasename");
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-if ($connection->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-} else {
-    printf("Connected to the database");
-}
-$connection->close();
+	function conectar()
+	{
+		$link = mysqli_connect("localhost", "root", "", "php_login");
+
+		if(!$link){
+			echo "Error: No se pudo establecer la conexion a la BD. ".PHP_EOL;
+			echo "Error de depuracion: ". mysqli_connect_errno(). PHP_EOL;
+			exit;
+		}
+		//echo "Exito de conexion" . PHP_EOL;
+		//echo "Informacion del host: ". mysqli_get_host_info($link). PHP_EOL;
+		return $link;
+	}
 ?>
